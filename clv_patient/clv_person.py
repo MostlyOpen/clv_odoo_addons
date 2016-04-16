2c2b15c42a752bab6c6f57747982920777402d6d
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2016-04-16 15:56:20 -0300

    Updated module "clv_patient".

--- clv_patient/clv_person.py
+++ clv_patient/clv_person.py
@@ -11,6 +11,7 @@ from openerp import models, fields
 class Person(models.Model):
     _inherit = 'clv_person'
 
+    code = fields.Char(size=64, string='Person/Patient Code', required=False)
     is_patient = fields.Boolean('Is Patient',
                                 help="If checked, the Person is a Patient.",
                                 default=0)
