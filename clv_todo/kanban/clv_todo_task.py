89fcdb47223cce36f9b10d8679c444ab95a67557
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2016-05-01 21:41:40 -0300

    Updated module "clv_todo".

--- clv_todo/kanban/clv_todo_task.py
+++ clv_todo/kanban/clv_todo_task.py
@@ -18,8 +18,22 @@
 #
 ###############################################################################
 
-from . import clv_todo_task
-from . import category
-from . import stage
-from . import wizard
-from . import kanban
+from openerp import models, fields
+
+
+class TodoTask(models.Model):
+    _inherit = 'clv_todo.task'
+    priority = fields.Selection(
+        [('0', 'Low'), ('1', 'Normal'), ('2', 'High')],
+        'Priority', default='0')
+    kanban_state = fields.Selection(
+        [('normal', 'In Progress'),
+         ('blocked', 'Blocked'),
+         ('done', 'Ready for next stage')],
+        'Kanban State', default='normal')
+    color = fields.Integer('Color Index')
+
+    company_id = fields.Many2one(
+        related='user_id.company_id',
+        string='Company',
+        store=True)
