dd962ef900faa000570dbfe86c298dec0b9e01fe
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2016-05-04 16:29:58 -0300

    Updated module "clv_medicament_list".

--- clv_medicament_list/item/clv_medicament_list_item.py
+++ clv_medicament_list/item/clv_medicament_list_item.py
@@ -1,30 +1,31 @@
-# -*- encoding: utf-8 -*-
-################################################################################
-#                                                                              #
-# Copyright (C) 2013-Today  Carlos Eduardo Vercelino - CLVsol                  #
-#                                                                              #
-# This program is free software: you can redistribute it and/or modify         #
-# it under the terms of the GNU Affero General Public License as published by  #
-# the Free Software Foundation, either version 3 of the License, or            #
-# (at your option) any later version.                                          #
-#                                                                              #
-# This program is distributed in the hope that it will be useful,              #
-# but WITHOUT ANY WARRANTY; without even the implied warranty of               #
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                #
-# GNU Affero General Public License for more details.                          #
-#                                                                              #
-# You should have received a copy of the GNU Affero General Public License     #
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.        #
-################################################################################
+# -*- coding: utf-8 -*-
+###############################################################################
+#
+# Copyright (C) 2013-Today  Carlos Eduardo Vercelino - CLVsol
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+###############################################################################
 
-from openerp import models, fields
+from openerp import models, fields, api
 
 
 class MedicamentListItem(models.Model):
     _name = 'clv_medicament_list.item'
 
     list_version_id = fields.Many2one('clv_medicament_list.version', string='Medicament List Version',
-                                      help='Medicament List Version', required=True)
+                                      help='Medicament List Version', required=False)
     medicament_id = fields.Many2one('clv_medicament', string='Medicament',
                                     help='Medicament', required=False)
     medicament_ref = fields.Reference([('clv_medicament', 'Medicament'),
@@ -47,4 +48,13 @@ class MedicamentListVersion(models.Model):
         'clv_medicament_list.item',
         'list_version_id',
         'Medicament List Itens'
-        )
+    )
+    count_items = fields.Integer(
+        'Number of Items',
+        compute='_compute_count_items'
+    )
+
+    @api.depends('medicament_list_item_ids')
+    def _compute_count_items(self):
+        for r in self:
+            r.count_items = len(r.medicament_list_item_ids)
